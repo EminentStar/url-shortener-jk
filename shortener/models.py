f6fbe52c4580592b8035f72c7a857377e6a403e9
JunkyuPark <junk3843@naver.com>
2016-11-04 16:00:38 +0900

    ShortUrl 모델 추가 및 admin사이트에 등록

--- shortener/models.py
+++ shortener/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+
+class ShortUrl(models.Model):
+    id = models.AutoField(primary_key=True)
+    short_url = models.CharField(max_length=20)
+    original_url = models.CharField(max_length=2083)

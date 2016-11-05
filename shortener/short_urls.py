b25419e045814f615c3d315d2a45b8eeb07ce1b0
JunkyuPark <junk3843@naver.com>
2016-11-06 01:11:15 +0900

    short url create 초안 구현

--- shortener/short_urls.py
+++ shortener/short_urls.py
@@ -1,7 +1,26 @@
+import uuid
+import redis
+
+from .base62_encoder import encode
+from .base62_encoder import decode
+from shortener.models import ShortUrl
 
 
-def create(orgin_url):
+redis_client = redis.StrictRedis(host='localhost', port=6379)
+
+
+def create(origin_url):
     """
         short URL을 생성하는 메서드
+        return:: short_url
     """
+    uuid_key = uuid.uuid4()
+    uuid_int = uuid_key.int
+    short_url = encode(uuid_int)
+   
+    su = ShortUrl(uuid=str(uuid_key), short_url=short_url, original_url=origin_url)
+    su.save()
 
+    redis_client.set(uuid_key, origin_url)
+    
+    return short_url
--- shortener/views.py
+++ shortener/views.py
@@ -2,11 +2,14 @@ from django.shortcuts import render
 from django.shortcuts import redirect
 from django.http import HttpResponse
 from django.http import HttpResponseRedirect
+import redis
+
 
 from .forms import UrlForm
+from .short_urls import create
+
 
 # Create your views here.
-
 
 def main_view(request):
     return HttpResponseRedirect('/create')
@@ -18,13 +21,17 @@ def create_view(request):
     
     if request.method == 'POST':
         origin_url = dict(request.POST)['url'][0]
+        short_url = create(origin_url)
+        
         print("Original URL: %s" % (origin_url))
+        print("Short URL: %s" % (short_url))
+
         dict_return['origin_url'] = origin_url
+        dict_return['short_url'] = short_url
 
     dict_return['form'] = form
 
     return render(request, 'shortener/main_view.html', dict_return)
-
 
 
 def shorturl_view(request, url):

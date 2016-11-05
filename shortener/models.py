0bbdf33dbbc0a6a6e3ae9dae330258f1c7483795
JunkyuPark <junk3843@naver.com>
2016-11-06 00:06:26 +0900

    DB 모델 수정(uuid 필드 추가)

--- shortener/models.py
+++ shortener/models.py
@@ -4,6 +4,7 @@ from django.db import models
 
 
 class ShortUrl(models.Model):
-    id = models.AutoField(primary_key=True)
+    uuid = models.CharField(max_length=36, primary_key=True, unique=True)
     short_url = models.CharField(max_length=20)
     original_url = models.CharField(max_length=2083)
+    
